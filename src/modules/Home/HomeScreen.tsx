5f137ced2b60a101a5820ef097973841bbdd15c1
Begzod Safarov <begzod.safarov07@gmail.com>
2023-04-11 10:16:24 +0500

    minor fixes

---
+++ src/modules/Home/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import Layout from '@/modules/common/Layout'
+import Hero from '../common/Hero/Hero'
+import AboutUsSection from './AboutUsSection'
+import ServicesSection from "./ServicesSection/index";
+import ForCustomerSection from "./ForCustomerSection";
+import BlogSection from "./BlogSection";
+
+export default function HomeScreen() {
+  return (
+    <Layout title="Al Bukhari Travel">
+      <Hero />
+      <AboutUsSection />
+      <ServicesSection />
+      <ForCustomerSection />
+      <BlogSection />
+    </Layout>
+  );
+}
