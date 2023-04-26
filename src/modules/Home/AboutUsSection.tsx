ea47670afb68860a0c1db0e82dbf377e3228a682
Umidjon Abdurakhmonov <misterabdurahmonov@gmail.com>
2023-04-26 16:16:30 +0500

    .

--- src/modules/Home/AboutUsSection.tsx
+++ src/modules/Home/AboutUsSection.tsx
@@ -10,12 +10,12 @@ const images = ['about_1', 'about_2']
 const AboutUsSection = () => {
   
   return (
-    <SectionsContainer className='xl:py-28 lg:py-20 xsm:py-20'>
+    <SectionsContainer className="xl:py-28 lg:py-20 xsm:py-20">
       {/* flexbox */}
-      <div className='relative flex xsm:flex-col lg:flex-row xl:items-start lg:items-center  h-fit'>
-        <div className='flex-[0.9] flex flex-col justify-between'>
+      <div className="relative flex xsm:flex-col lg:flex-row xl:items-start lg:items-center  h-fit">
+        <div className="flex-[0.9] flex flex-col justify-between">
           <SupTitle>ABOUT US</SupTitle>
-          <Title className='xsm:text-3xl'>
+          <Title className="xsm:text-3xl">
             Explore All Corners of The World With Us
           </Title>
           {/* paras */}
@@ -27,36 +27,37 @@ const AboutUsSection = () => {
               large language ocean.
             </Para>
             <Para>
-              A sm river named Duden flows by their place and supplies it
-              with the necessary regelialia. It is a paradisematic country, in
-              which roasted parts of sentences fly into your mouth.
+              A sm river named Duden flows by their place and supplies it with
+              the necessary regelialia. It is a paradisematic country, in which
+              roasted parts of sentences fly into your mouth.
             </Para>
           </div>
           {/* button container */}
-          <div className='pt-10 pb-20'>
+          <div className="pt-10 pb-20">
             <FullButton>Read More</FullButton>
           </div>
         </div>
-        <div className='relative flex-[1.1] xl:pl-10 lg:pl-10 md:pl-0 sm:pl-0 flex justify-center lg:space-x-5 lg:items-center xsm:space-x-2 h-full pb-12'>
+        <div className="relative flex-[1.1] xl:pl-10 lg:pl-10 md:pl-0 sm:pl-0 flex justify-center lg:space-x-5 lg:items-center xsm:space-x-2 h-full pb-12">
           {images.map((image: string, i: number) => (
             <div
               key={i}
               className={` ${
-                i === 1 ? 'lg:pt-8' : 'pt-0'
+                i === 1 ? "lg:pt-8" : "pt-0"
               } md:pt-0 sm:pt-0 xsm:pt-0`}
             >
+              {/*eslint-disable-next-line @next/next/no-img-element */}
               <img
                 src={`/images/${image}.png`}
-                alt='Bukhari Travel'
-                width={'auto'}
-                height={'100%'}
-                loading='lazy'
+                alt="Bukhari Travel"
+                width={"auto"}
+                height={"100%"}
+                loading="lazy"
               />
             </div>
           ))}
         </div>
       </div>
     </SectionsContainer>
-  )
+  );
 }
 export default AboutUsSection;
