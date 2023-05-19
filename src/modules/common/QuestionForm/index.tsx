8538c8c50f46e315f19a85923d3e8f900631d46e
Umidjon Abdurakhmonov <misterabdurahmonov@gmail.com>
2023-05-19 23:45:14 +0500

    minor improvement in contact page

--- src/modules/common/QuestionForm/index.tsx
+++ src/modules/common/QuestionForm/index.tsx
@@ -14,19 +14,19 @@ const QuestionForm = ({
         <input
           type="text"
           placeholder="Your name"
-          className="flex text-left justify-start items-start focus:outline-none focus:border-orange border px-4 md:px-8 w-[45%] py-2"
+          className="flex text-left justify-start items-start rounded-lg border-gray focus:outline-none focus:border-orange border px-4 md:px-8 w-[45%] py-2"
         />
         <input
           type="text"
           placeholder="Your email"
-          className="focus:outline-none focus:border-orange border px-4 md:px-8 w-[45%] py-2"
+          className="focus:outline-none focus:border-orange rounded-lg border border-gray px-4 md:px-8 w-[45%] py-2"
         />
       </div>
       <div>
         <input
           type="text"
           placeholder="Subject"
-          className="focus:outline-none focus:border-orange border px-4 md:px-8 py-2 mt-4 w-full"
+          className="focus:outline-none border-gray focus:border-orange rounded-lg border px-4 md:px-8 py-2 mt-4 w-full"
         />
       </div>
       <div>
@@ -34,11 +34,12 @@ const QuestionForm = ({
           cols={30}
           rows={7}
           placeholder="Message"
-          className="focus:outline-none focus:border-orange border px-4 md:px-8 py-2  w-full mt-4 mb-3 flex justify-start text-start h-48"
+          className="focus:outline-none border-gray focus:border-orange rounded-lg border px-4 md:px-8 py-2  w-full mt-4 mb-3 flex justify-start text-start h-48"
         />
       </div>
-
-      <FullButton onClick={handleClick}>{buttonText}</FullButton>
+      <div className="flex justify-end">
+        <FullButton onClick={handleClick} className="">{buttonText}</FullButton>
+      </div>
     </form>
   );
 };
